1cf4eb672470995ad0e0ef29e976dc22216e2131
bhargav-patel-07 <bhargavpatel07100@gmail.com>
2026-03-01 14:21:06 +0530

    minor improvement

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Ai04 from "@/components/homeaiInput";
-import GitButton from "@/components/ui/github";
+import Ai04 from "@/components/ui/homeaiInput";
 
 export default function Home() {
   return (
--- apps/web/components/ui/homeaiInput.tsx
+++ apps/web/components/ui/homeaiInput.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "./dropdown-menu";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Textarea } from "@/components/ui/textarea";
